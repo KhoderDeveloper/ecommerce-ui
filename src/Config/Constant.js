c4cd7f0d886a7042c5f6adcb993a5e8d06addd04
KhoderDeveloper <khoder.osman.92@gmail.com>
2023-08-01 22:38:14 +0300

    resolve conflict

--- src/Config/Constant.js
+++
@@ -1,8 +0,0 @@
-const Constant = {
-    // serverlink: 'http://localhost:4000',
-    serverlink: 'https://smiling-coveralls-crow.cyclic.app/',
-    newserverlink : 'https://sport-sphere-080b4425404a.herokuapp.com/',
-    timeout: 40000
-};
-
-export default Constant;
